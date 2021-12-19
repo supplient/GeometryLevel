2a926858d5d00704c25bb7d75106c3b00acf1860
supplient <437566830@qq.com>
2021-12-19 16:57:58 +0800

    实现关卡切换 * 添加GoalEffector * 添加LevelManager

--- Assets/Scripts/Components/Manager/LevelManager.cs
+++ Assets/Scripts/Components/Manager/LevelManager.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace geo_level
 {
@@ -17,10 +19,48 @@ namespace geo_level
 				return gameObject.GetComponent<LevelManager>();
 			}
 		}
+
+		private AsyncOperation m_asyncLoadForNextLevel = null;
+
+		private void Start()
+		{
+			var nowScene = SceneManager.GetActiveScene();
+			int nowIndex = nowScene.buildIndex;
+			int nextIndex = (nowIndex + 1) % SceneManager.sceneCountInBuildSettings;
+			LoadLevel(nextIndex);
+		}
+
+		private void LoadLevel(int buildIndex)
+		{
+
+			Debug.Log(String.Format("Start Loading Level {0}", buildIndex));
+
+			m_asyncLoadForNextLevel = SceneManager.LoadSceneAsync(buildIndex);
+			m_asyncLoadForNextLevel.allowSceneActivation = false;
+		}
 		
 		public void CompleteLevel()
 		{
 			Debug.Log("Level Completed.");
+			var nowScene = SceneManager.GetActiveScene();
+			int nowIndex = nowScene.buildIndex;
+
+			// We wait by a corotinue to await long-time no response.
+			m_asyncLoadForNextLevel.allowSceneActivation = true;
+			StartCoroutine(WaitLoadingNextLevel(nowIndex));
+		}
+
+		private IEnumerator WaitLoadingNextLevel(int nowIndex)
+		{
+			while(!m_asyncLoadForNextLevel.isDone)
+				yield return null;
+
+			SceneManager.UnloadSceneAsync(nowIndex);
+		}
+
+		public void CompleteGame()
+		{
+			Debug.Log("Game Completed.");
 			// TODO
 		}
 	}
